a14a73f57dee99818233a074d982e00513b6cd27
Your Name <you@example.com>
2022-03-01 16:01:44 +0100

    NAHRAVANI Z UCEBNY

---
+++ Labs/03-vivado/vivado/comparator_skapik/comparator_skapik.srcs/sources_1/new/comparator_skapik.vhd
@@ -0,0 +1,28 @@
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+------------------------------------------------------------
+-- Entity declaration for 2-bit binary comparator
+------------------------------------------------------------
+entity comparator_skapik is
+    port(
+        b_i           : in  std_logic_vector(4 - 1 downto 0);
+        a_i           : in  std_logic_vector(4 - 1 downto 0);
+        B_greater_A_o : out std_logic;
+        B_equals_A_o  : out std_logic;
+        B_less_A_o    : out std_logic  
+    );
+end entity comparator_skapik ;
+
+------------------------------------------------------------
+-- Architecture body for 2-bit binary comparator
+------------------------------------------------------------
+architecture Behavioral of comparator_skapik  is
+begin
+    B_greater_A_o <= '1' when (b_i > a_i) else '0';
+    B_equals_A_o  <= '1' when (b_i = a_i) else '0';
+    B_less_A_o  <= '1' when (b_i < a_i) else '0';
+
+end architecture Behavioral;
